393f9180ad25aa6913f1c5a460db501e5b276233
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-17 18:20:53 +0200

    Create basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+
+  before_save :downcase_email
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+end
