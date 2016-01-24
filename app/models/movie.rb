432e06ef946e5915faea5f12b5c0ad46b76fe046
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-24 13:10:56 +0200

    Add scope for selecting movies for users

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,8 @@ class Movie < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
   validates :year, presence: true, inclusion: 1800..2100
+
+  scope :for_users, -> (user_ids) {
+    joins(:users).where('movies_users.user_id IN (?)', user_ids)
+  }
 end
