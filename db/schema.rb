fefa2c248e0e43b7a48b895b9b86258a4ffe712e
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-26 18:48:09 +0200

    Rename movies_users table to user_movies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122162221) do
+ActiveRecord::Schema.define(version: 20160126133842) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -21,12 +21,12 @@ ActiveRecord::Schema.define(version: 20160122162221) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "movies_users", id: false, force: :cascade do |t|
+  create_table "user_movies", id: false, force: :cascade do |t|
     t.integer "movie_id"
     t.integer "user_id"
   end
 
-  add_index "movies_users", ["movie_id", "user_id"], name: "index_movies_users_on_movie_id_and_user_id"
+  add_index "user_movies", ["movie_id", "user_id"], name: "index_user_movies_on_movie_id_and_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
