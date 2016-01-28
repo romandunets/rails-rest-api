a36a5d1fb8b20e09a6b89b5b8a2f4b0cfde2da33
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-28 17:21:32 +0200

    Add functional test for getting one user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,18 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   def setup
+    @user = User.create(email: 'testuser@mail.com')
     @controller = Api::UsersController.new
   end
 
   test 'should get index' do
-    get(:index, format: :json)
+    get :index, format: :json
     assert_response :success
-    assert_not_nil assigns(:users)
+    assert_not_nil assigns :users
+  end
+
+  test 'should show user' do
+    get :show, id: @user, format: :json
+    assert_response :success
   end
 end
