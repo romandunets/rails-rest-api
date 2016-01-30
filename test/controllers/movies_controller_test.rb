f70118d9725fcb3e43bcba586df0ee899ab0d429
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-30 16:54:43 +0200

    Improve tests for movies controller

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -8,25 +8,32 @@ class MoviesControllerTest < ActionController::TestCase
 
   test 'should get index' do
     get :index, format: :json
+    body = JSON.parse(response.body)
     assert_response :success
-    assert_not_nil assigns :movies
+    assert_equal 1, body['movies'].count
   end
 
   test 'should show movie' do
     get :show, id: @movie, format: :json
+    body = JSON.parse(response.body)
     assert_response :success
+    assert_equal @movie.id, body['movie']['id']
   end
 
   test 'should create movie' do
     assert_difference('Movie.count') do
       post :create, title: 'whitescrewdriver', year: 2003, format: :json
+      body = JSON.parse(response.body)
       assert_response :success
+      assert_not_nil assigns body['id']
     end
   end
 
   test 'should update movie' do
-    patch :update, id: @movie, post: { title: 'yellowhammer' }, format: :json
+    patch :update, id: @movie, title: 'yellowhammer', format: :json
+    body = JSON.parse(response.body)
     assert_response :success
+    assert_equal 'yellowhammer', body['movie']['title']
   end
 
   test 'should destroy movie' do
@@ -37,8 +44,11 @@ class MoviesControllerTest < ActionController::TestCase
   end
 
   test 'should get movie users' do
+    @user = User.create(email: 'testuser@mail.com')
+    @user.movies << @movie
     get :users, id: @movie, format: :json
+    body = JSON.parse(response.body)
     assert_response :success
-    assert_not_nil assigns :movie
+    assert_equal 1, body['users'].count
   end
 end
