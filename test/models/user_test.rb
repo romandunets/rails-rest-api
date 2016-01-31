607ffcf687384e134a0677febfde0b9a4dea3170
Roman Dunets <roman.dunets.email@gmail.com>
2016-01-31 12:07:55 +0200

    Add unit test for email validation which should reject invalid addresses in user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,4 +18,12 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 250 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user.@example.com@example.com user@exam+ple.com user@example,com user_at_example.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
