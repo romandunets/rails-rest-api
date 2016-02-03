c9f4315e19ef1856dd3688e5df9bcef43fbbb8e5
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-03 17:26:14 +0200

    Update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202204949) do
+ActiveRecord::Schema.define(version: 20160202210149) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 20160202204949) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.date     "birthday"
+    t.string   "gender"
+    t.text     "description"
+    t.string   "address"
+    t.string   "postal_code"
+    t.string   "city"
+    t.string   "country"
   end
 
 end
