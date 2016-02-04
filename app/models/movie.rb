a031f20ca14a4b834bd974b3ef0fcae78e105bf8
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-04 17:26:32 +0200

    Add validation for duration column in movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,6 @@ class Movie < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
   validates :year, presence: true, inclusion: 1800..2100
+  validates :duration, presence: true, inclusion: 0..21600
   validates :studio, presence: true, length: { maximum: 255 }
 end
