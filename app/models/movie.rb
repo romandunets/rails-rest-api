7334b91b9ca7726338a1ee7e4005222ed1f6a4a4
Roman Dunets <roman.dunets.email@gmail.com>
2016-02-04 17:24:10 +0200

    Add validation for studio in movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,5 @@ class Movie < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
   validates :year, presence: true, inclusion: 1800..2100
+  validates :studio, presence: true, length: { maximum: 255 }
 end
